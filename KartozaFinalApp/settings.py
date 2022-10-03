e5e55ea8c5a6aadc37674dd1d856bc7ae0d53030
Blessed Mutengwa <blessedmutengwa@gmail.com>
2022-10-03 16:28:59 +0200

    added user and portfolio apps to installed apps

--- KartozaFinalApp/settings.py
+++ KartozaFinalApp/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Our apps
+    "portfolio",
+    "users",
 ]
 
 MIDDLEWARE = [
